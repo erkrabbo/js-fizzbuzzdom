e7b810faad1683198643d75ae808f51782d142df
Luca Andaloro <luca.andaloro4@gmail.com>
2022-02-23 17:16:30 +0100

    end

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-let container = document.body.querySelector(`.container`);
-    container.style = `display: flex; width: 100%; flex-wrap: wrap;`;
+const fizzBuzzContainer = document.body.querySelector(`.container`);
+const container = document.querySelector(".container");
+
+fizzBuzzContainer.style = `display: flex; width: 100%; flex-wrap: wrap;`;
 
 aHundred(1, 100);
 
 
 function aHundred(start, end){
     for(i = start; i <= end ; i++){
-
-        let container = document.querySelector(".container");
-        let element = document.createElement('div');
+        
+        const element = document.createElement('div');
 
         if (!(i % 3) && !(i % 5)){
-            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: blue;`;
+           
+            element.setAttribute("class", "blue box");
             element.innerHTML=`<span>FizzBuzz<\span>;`;
 
             container.appendChild(element);
         }
         else if( !(i % 3) ){
-            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: yellow;`;
+           
+            element.setAttribute("class", "yellow box");
             element.innerHTML=`<span>Fizz<\span>`;
 
             container.appendChild(element);
         }else if( !(i % 5) ){
-            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: red;`;
+           
+            element.setAttribute("class", "red box");
             element.innerHTML=`<span>Buzz<\span>`;
 
             container.appendChild(element);
         }else{
-            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: lightblue;`;
+           
+            element.setAttribute("class", "lightblue box");
             element.innerHTML=`<span>${i}<\span>`;
 
             container.appendChild(element);
