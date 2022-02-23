64d2e80b6c8c60610fb74238109836d0280acc9e
Luca Andaloro <luca.andaloro4@gmail.com>
2022-02-23 16:48:43 +0100

    MILESTONE 3

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-aHundred();
+let container = document.body.querySelector(`.container`);
+    container.style = `display: flex; width: 100%; flex-wrap: wrap;`;
 
-function aHundred(){
-    for(i = 1; i < 101; i++){
+aHundred(1, 100);
+
+
+function aHundred(start, end){
+    for(i = start; i <= end ; i++){
+
+        let container = document.querySelector(".container");
+        let element = document.createElement('div');
 
         if (!(i % 3) && !(i % 5)){
-            console.log("BuzzFizz");
+            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: blue;`;
+            element.innerHTML=`<span>FizzBuzz<\span>;`;
+
+            container.appendChild(element);
         }
-        else if(!(i%3)){
-            console.log("Fizz");
-        }else if(!(i%5)){
-            console.log("Buzz")
+        else if( !(i % 3) ){
+            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: yellow;`;
+            element.innerHTML=`<span>Fizz<\span>`;
+
+            container.appendChild(element);
+        }else if( !(i % 5) ){
+            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: red;`;
+            element.innerHTML=`<span>Buzz<\span>`;
+
+            container.appendChild(element);
         }else{
-            console.log(i);
+            element.style=`width: 20%; height: 100px; display: flex; align-items: center; justify-content: center; background-color: lightblue;`;
+            element.innerHTML=`<span>${i}<\span>`;
+
+            container.appendChild(element);
         }
     }
 }
